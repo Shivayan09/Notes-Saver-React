f1e8fd1c8d1abcf7c7616af3c9ba01d10ea4ad61
Shivayan Chakravarty <147698014+Shivayan09@users.noreply.github.com>
2025-06-01 13:34:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      external: ['fs', 'path', 'os'], // prevent Vercel from choking
+    }
+  },
+  define: {
+    'process.env': {}, // required to avoid undefined errors on Vercel
+  }
 })
